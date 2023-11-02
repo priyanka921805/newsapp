b9453eca655f107074c98e10abf6885bf862c42e
priyanka921805 <priyanka921805verma@gmail.com>
2023-11-02 12:06:15 +0530

    adding my newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import NewsItem from './NewsItem'
 
 export class News extends Component {
   render() {
     return (
       <div>
         this is a news components.
+        <NewsItem/>                                {/* ctrl+d */}
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
       </div>
     )
   }
---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        this is a newsitem components 3.
+      </div>
+    )
+  }
+}
+
+export default NewsItem
