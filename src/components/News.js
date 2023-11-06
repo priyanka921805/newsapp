868b60cd0632e596fd8b6ef1d9a90846b993413c
priyanka921805 <priyanka921805verma@gmail.com>
2023-11-06 12:57:23 +0530

    adding my NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is a news components.
-        <NewsItem/>                                {/* ctrl+d */}
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>                                     {/* ctrl+d */}
+        <h2>NewsMonkey - Top Headlines</h2>
+        <div className='row'>
+          <div className='col-md-4'>
+          <NewsItem title="myTitle" description="mydesc" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/44EC/production/_95244671_mediaitem95244670.jpg"/>  
+          </div>                                    
+          <div className='col-md-4'>
+          <NewsItem title="myTitle" description="mydesc" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/44EC/production/_95244671_mediaitem95244670.jpg"/>  
+          </div>
+          <div className='col-md-4'>
+          <NewsItem title="myTitle" description="mydesc" imageUrl="https://ichef.bbci.co.uk/news/1024/branded_news/44EC/production/_95244671_mediaitem95244670.jpg"/>  
+          </div>
+        </div>
       </div>
     )
   }
