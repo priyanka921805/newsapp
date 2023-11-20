5bb1f5896e6aba34070a2bb214d9700458db861e
priyanka921805 <priyanka921805verma@gmail.com>
2023-11-20 11:30:56 +0530

    adding loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -14,7 +14,7 @@ export class News extends Component {
 
   async componentDidMount(){
     console.log("cdn");
-    let url ="https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=1&pageSize=20";
+    let url =`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
@@ -22,7 +22,7 @@ export class News extends Component {
   }
     handlePrevClick = async ()=>{
       console.log("Previous");
-      let url =`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=${this.state.page - 1}&pageSize=20`;
+      let url =`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -34,11 +34,11 @@ export class News extends Component {
 
     handleNextClick = async ()=>{
         console.log("Next");
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
 
         }
         else{
-        let url =`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=${this.state.page + 1}&pageSize=20`;
+        let url =`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=580f047e7ec44e39bfee24ff1a006b2a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
@@ -48,13 +48,10 @@ export class News extends Component {
       })
     }
   }
-
-
-
   render() {
     return (
       <div className='container my-3'>                                     {/* ctrl+d */}
-        <h3>NewsMonkey - Top Headlines</h3>
+        <h3 className='text-center'>NewsMonkey - Top Headlines</h3>
         <div className='row'>
           {this.state.articles.map((element)=>{
             return <div className='col-md-4'key={element.url}>
@@ -65,7 +62,7 @@ export class News extends Component {
         </div>
         <div className='container d-flex justify-content-between'>
         <button disabled={this.state.page<=1}  type="button" className="btn btn-primary" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr; </button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </div>
     )
